3c2356b060624b669eafa8b26c2b7de3a15917dd
Ferrete <erferrete3@gmail.com>
2018-10-13 17:12:24 +0200

    Caso 3

--- L01 Workspace/Acme-Shout/src/main/java/controllers/CustomerController.java
+++ L01 Workspace/Acme-Shout/src/main/java/controllers/CustomerController.java
@@ -10,15 +10,25 @@
 
 package controllers;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import services.ShoutService;
+import domain.Shout;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController extends AbstractController {
 
 	// Constructors -----------------------------------------------------------
+	@Autowired
+	private ShoutService	shoutService;
+
 
 	public CustomerController() {
 		super();
@@ -26,21 +36,22 @@ public class CustomerController extends AbstractController {
 
 	// Action-1 ---------------------------------------------------------------		
 
-	@RequestMapping("/action-1")
+	@RequestMapping(value = "/action-1", method = RequestMethod.GET)
 	public ModelAndView action1() {
 		ModelAndView result;
-
+		Collection<Shout> shouts;
+		shouts = this.shoutService.findAll();
 		result = new ModelAndView("customer/action-1");
-
+		result.addObject("shouts", shouts);
 		return result;
 	}
 
 	// Action-2 ---------------------------------------------------------------		
 
-	@RequestMapping("/action-2")
-	public ModelAndView action2() {
+	@RequestMapping(value = "/action-2", method = RequestMethod.GET)
+	public ModelAndView action2Get() {
 		ModelAndView result;
-
+		final Shout shout;
 		result = new ModelAndView("customer/action-2");
 
 		return result;
