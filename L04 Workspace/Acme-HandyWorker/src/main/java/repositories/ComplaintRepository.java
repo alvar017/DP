2179b0740068757296941d03a0af733b0fd9e772
Álvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2018-11-27 18:17:07 +0100

    Backup

--- L04 Workspace/Acme-HandyWorker/src/main/java/repositories/ComplaintRepository.java
+++ L04 Workspace/Acme-HandyWorker/src/main/java/repositories/ComplaintRepository.java
@@ -18,6 +18,12 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
 	@Query("select c from Complaint c join c.referee r where r.id = ?1")
 	Collection<Complaint> getComplaintRefereeId(Integer id);
 
-	@Query("select f.complaint from HandyWorker h join h.finder f where h.id=?1")
-	Collection<Complaint> getComplaintFixUpByHandyWorker(Integer id);
+	//	@Query("select f.complaint from HandyWorker h join h.finder f where h.id=?1")
+	//	Collection<Complaint> getComplaintFixUpByHandyWorker(Integer id);
+
+	@Query("select c from Complaint c join c.fixUp where c.fixUp.handyWorker.id = ?1")
+	Collection<Complaint> getComplaintFixUpByHandyWorker2(Integer Hwid);
+
+	@Query("select c from Complaint c where c.referee=null")
+	Collection<Complaint> getComplaintWithoutReferee(Integer id);
 }
